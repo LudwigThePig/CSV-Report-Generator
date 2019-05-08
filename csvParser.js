c647bfea3ea333217c876a63a68dc718495098d4
Morgan Galvin <morgan@ludwigthepig.com>
2019-05-07 18:39:47 -0600

    added ids to each row

--- csvParser.js
+++ csvParser.js
@@ -1,10 +1,15 @@
 module.exports = (input) => {
   input = JSON.parse(input);
   const headers = Object.keys(input).slice(0, -1);
-  // headers.unshift('id');
+  headers.unshift('id');
+  let counter = 0;
   let csv = [headers]
   const recurse = (node) => {
     const row = Object.keys(node).slice(0, -1).map(key => node[key]);
+    //add id
+    row.unshift(counter.toString());
+    counter++;
+
     csv.push(row);
     if (node.children !== []) {
       node.children.forEach(child => recurse(child));
