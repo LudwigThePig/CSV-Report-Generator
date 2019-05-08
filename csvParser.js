e977d787f92458e0c90805c19b3c57e34362e1ae
Morgan Galvin <morgan@ludwigthepig.com>
2019-05-07 18:48:06 -0600

    Keys unique to headers are now stored in 'other'

--- csvParser.js
+++ csvParser.js
@@ -2,15 +2,29 @@ module.exports = (input) => {
   input = JSON.parse(input);
   const headers = Object.keys(input).slice(0, -1);
   headers.unshift('id');
+  headers.push('other');
   let counter = 0;
   let csv = [headers]
   const recurse = (node) => {
-    const row = Object.keys(node).slice(0, -1).map(key => node[key]);
-    //add id
+    let others = [];
+    const row = Object.keys(node).slice(0, -1).map(key =>{
+      //check for unique keys
+      if (!headers.includes(key)) {
+        others.push(node[key]);
+      } else {
+        return node[key];
+      }
+    });
+    
+    //add id and other to the row
     row.unshift(counter.toString());
     counter++;
+    row.push(others.join(" "));
 
+    //Push it!
     csv.push(row);
+
+    //if children, parse recursively
     if (node.children !== []) {
       node.children.forEach(child => recurse(child));
     } else {
